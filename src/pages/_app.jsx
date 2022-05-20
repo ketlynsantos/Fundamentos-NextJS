7d6adaeb5caf35ffef62932113b1dd051eda7fb1
Ketlyn Santos <ketlyncastro2004@gmail.com>
2022-05-19 21:14:38 -0300

    Create project

---
+++ src/pages/_app.jsx
@@ -0,0 +1,11 @@
+import '../styles/globals.css'
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+//Next JS é a versão framework do React
+//Acrescente uma séries de funcionalidades e estrutras no React
+//Framework - basicamente uma estrutura pré pronta
+
+export default MyApp
